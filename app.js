243bb00ed87fa067d256f1d15aa32c87cf912845
MuhammadMoizRafiq <m4moiz2000@gmail.com>
2022-10-31 14:38:28 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// alert( "My Name Is Moiz" );
+
+// var name = "Mark";
+// var x = "Mark"
+
+// var nationality
+// nationality = "US" 
+
+// var firstname = "Moiz";
+// alert(firstname);
+
+// var weight = 150;
+
+// var orignalnumber = 40;
+// var newnum = 30;
+// var added = orignalnumber + newnum ;
+
+// var number = 2 + 2;
+// var add = 34;
+// var add1 = 21;
+// var added = add + add1;
+
+// var num = 34 * 4;
+// var num = 23 - 2;
+
+// var num = 2;
+// var newnum = ++2;
+// var newnum = --2;
+// var newnum = 2--;
+// var newnum = 2++;
+
+// var num = 2+4 (3 * 2);
+// var num = (3 * 2)  43 - 5;
+
+
+// var message = "Hello,";
+// var username = "Moiz";
+// var mark = "!";
+// var custom = message + username + mark;
+// alert(custom);
